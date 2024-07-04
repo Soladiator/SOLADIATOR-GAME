8aa0352af591210294906e8cf2447b243db50d41
OguzhanUmutOzgur <oguzhanumut@icloud.com>
2024-07-04 05:47:36 +0300

    Combat and Stats helper types

--- src/types/combat.ts
+++ src/types/combat.ts
@@ -1,12 +1,44 @@
-import {Character} from "@prisma/client";
+import {Character, Item} from "@prisma/client";
+import {CharacterStats, CombatStats} from "./stats";
 
-enum HitType {
+export enum HitType {
   Normal = "Normal",
   Critical = "Critical",
-  Miss = "Miss",
 }
 
-interface CombatEntity extends Character {
-  damage?: number;
-  damageReduction?: number;
+export enum DefenseType {
+  Dodge = "Dodge",
+  Block = "Block",
+  Normal = "Normal",
+}
+
+export interface ComputedCombatEntity extends Character {
+  combatStats: CombatStats;
+  maxHealth: number;
+  stats: CharacterStats;
+}
+
+export interface AttackResult {
+  damage: number;
+  hitType: HitType;
+}
+
+export interface DefenseResult {
+  defendType: DefenseType;
+  incomingDamage: number;
+  health: number;
+}
+
+export interface CombatRoundResult {
+  attacker: string;
+  attackResult: AttackResult;
+  defender: string;
+  defendResult: DefenseResult;
+  round: number;
+}
+
+export interface CombatResult {
+  rounds: CombatRoundResult[];
+  winner: ComputedCombatEntity;
+  loser: ComputedCombatEntity;
 }
