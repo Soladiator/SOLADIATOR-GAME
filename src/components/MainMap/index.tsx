7f779ec3f508c496261ffa645c0dc9f7bb671e87
boranfurkan <furkanboran123@gmail.com>
2024-07-05 18:33:57 +0300

    feat: update main map

--- src/components/MainMap/index.tsx
+++ src/components/MainMap/index.tsx
@@ -1,65 +1,65 @@
-"use client";
+'use client'
 
-import React, { MutableRefObject, useEffect, useRef, useState } from "react";
-import { useDraggable } from "react-use-draggable-scroll";
-import ImageMapper, { CustomArea, Map } from "react-img-mapper";
-import MapBuildingModal from "../Modals/MapBuildingModals";
-import { URL, MAP, Area } from "@/constants/Map";
-import { LoadingBackdrop } from "@/components/Backdrops/LoadingBackdrop";
-import Image from "next/image";
-import BuildingLabels from "../BuildingLabels";
+import React, { MutableRefObject, useEffect, useRef, useState } from 'react'
+import { useDraggable } from 'react-use-draggable-scroll'
+import ImageMapper, { CustomArea, Map } from 'react-img-mapper'
+import MapBuildingModal from '../Modals/MapBuildingModals'
+import { URL, MAP, Area } from '@/constants/Map'
+import { LoadingBackdrop } from '@/components/Backdrops/LoadingBackdrop'
+import Image from 'next/image'
+import BuildingLabels from '../BuildingLabels'
 
 const MainMap = () => {
-  const [innerWidth, setInnerWidth] = useState<number>(0);
-  const [scale, setScale] = useState<number>(1);
-  const [area, setArea] = useState<CustomArea | Area | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [innerWidth, setInnerWidth] = useState<number>(0)
+  const [scale, setScale] = useState<number>(1)
+  const [area, setArea] = useState<CustomArea | Area | null>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
   const parentRef =
-    useRef<HTMLDivElement>() as MutableRefObject<HTMLInputElement>;
+    useRef<HTMLDivElement>() as MutableRefObject<HTMLInputElement>
   const { events } = useDraggable(parentRef, {
     applyRubberBandEffect: true,
-  });
+  })
 
   useEffect(() => {
     const updateDimensions = () => {
-      if (typeof window !== "undefined") {
-        const width = window.innerWidth;
+      if (typeof window !== 'undefined') {
+        const width = window.innerWidth
         if (width < 640) {
-          setInnerWidth(width * 3.848);
+          setInnerWidth(width * 3.848)
         } else if (width < 769) {
-          setInnerWidth(width * 1.8);
+          setInnerWidth(width * 1.8)
         } else {
-          setInnerWidth(width * 1.3);
+          setInnerWidth(width * 1.3)
         }
-        setScale(width / innerWidth);
+        setScale(width / innerWidth)
       }
-    };
+    }
 
-    updateDimensions();
-    window.addEventListener("resize", updateDimensions);
-    return () => window.removeEventListener("resize", updateDimensions);
-  }, [innerWidth]);
+    updateDimensions()
+    window.addEventListener('resize', updateDimensions)
+    return () => window.removeEventListener('resize', updateDimensions)
+  }, [innerWidth])
 
   useEffect(() => {
     if (parentRef.current) {
       parentRef.current.scrollLeft =
-        innerWidth / 2 - parentRef.current.clientWidth / 2;
+        innerWidth / 2 - parentRef.current.clientWidth / 2
 
       setTimeout(() => {
         parentRef.current.scrollLeft =
-          innerWidth / 2 - parentRef.current.clientWidth / 2;
-      }, 550);
+          innerWidth / 2 - parentRef.current.clientWidth / 2
+      }, 550)
     }
-  }, [innerWidth, isLoading]);
+  }, [innerWidth, isLoading])
 
   const handleAreaMouseEnter = () => {
-    document.body.style.cursor = "pointer";
-  };
+    document.body.style.cursor = 'pointer'
+  }
 
   const handleAreaMouseLeave = () => {
-    document.body.style.cursor = "default";
-  };
+    document.body.style.cursor = 'default'
+  }
 
   return (
     <div
@@ -67,7 +67,7 @@ const MainMap = () => {
       ref={parentRef}
       onMouseDown={(e) => {
         if (!area) {
-          events.onMouseDown(e);
+          events.onMouseDown(e)
         }
       }}
     >
@@ -82,7 +82,7 @@ const MainMap = () => {
             layout="fill"
             className="object-center object-cover pointer-events-none"
             src={URL}
-            alt={"Map"}
+            alt={'Map'}
           />
           <div className="relative z-1">
             <LoadingBackdrop />
@@ -91,7 +91,7 @@ const MainMap = () => {
       )}
 
       <div
-        className={`${!isLoading && "relative"}`}
+        className={`${!isLoading && 'relative'}`}
         style={{
           width: `${innerWidth}px`,
         }}
@@ -104,7 +104,7 @@ const MainMap = () => {
           strokeColor="rgba(92, 76, 250, 0.4)"
           lineWidth={4}
           onClick={(areaDetails) => {
-            setArea(areaDetails);
+            setArea(areaDetails)
           }}
           onMouseEnter={handleAreaMouseEnter}
           onMouseLeave={handleAreaMouseLeave}
@@ -120,7 +120,7 @@ const MainMap = () => {
         <MapBuildingModal selectedArea={area as Area} setArea={setArea} />
       )}
     </div>
-  );
-};
+  )
+}
 
-export default MainMap;
+export default MainMap
